1b1121443b3c8d2e498067438f830bb7f3a6c944
MorFyNC <bulat.hisambiev@gmail.com>
2025-01-20 20:49:27 +0300

    Добавьте файлы проекта.

---
+++ TestBlazor3K/ApiRequest/Model/User.cs
@@ -0,0 +1,35 @@
+﻿namespace TestBlazor3K.ApiRequest.Model
+{
+    public class UserDataShort
+    {
+        public int id_User { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool isAdmin { get; set; }
+    }
+
+    public class UserData
+    {
+       public bool status { get; set; }
+       public UserDataContainer data { get; set; }
+    }
+
+    public class UserDataContainer
+    {
+        public List<UserDataShort> users { get; set; }
+    }
+
+    public class ReqDataUser
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserAddData
+    {
+        public bool status { get; set; }
+        public string message { get; set; }
+    }
+}
---
+++ TestBlazor3K/UserContext/UserContext.cs
@@ -0,0 +1,10 @@
+﻿using TestBlazor3K.ApiRequest.Model;
+
+namespace TestBlazor3K.UserContext
+{
+    public class UserContext
+    {
+        public UserDataShort CurrentUser { get; set; }
+        public bool IsCurrentUserAdmin { get => CurrentUser.isAdmin; }
+    }
+}
